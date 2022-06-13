b70ebdfd3fc6d1b582a769288e8daa01ecc596f8
ReggieCodes123 <johnsonreginald12@yahoo.com>
2022-06-13 15:41:22 -0400

    Create inches_feet_converter.py

---
+++ inches_feet_converter.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 13 15:27:58 2022
+
+@author: Reginald Johnson
+"""
+inches= 12
+feet = 7
+
+inches_to_feet = inches /12
+feet_to_inches = feet * 12
+
+print (feet, "feet is ", feet_to_inches, " inches")
+print (inches, "inches is ", inches_to_feet, " feet")
